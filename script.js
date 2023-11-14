ea58fdd8e630c8f1f225bb5bf04091779d204b57
Adriano Garcia <adrianogtlopes@gmail.com>
2023-11-14 19:44:26 -0300

    tablet query done

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-function navMenu() {
-  const navBar = document.getElementById("nav-bar")
-  const navBtn = document.querySelector(".material-symbols-rounded")
+const navBar = document.getElementById("nav-bar")
+const navBtn = document.querySelector(".material-symbols-rounded")
+
+function touchMenu() {
   if (navBar.style.display == "none") {
     navBar.style.display = "block"
     navBtn.textContent = "close"
   } else {
     navBar.style.display = "none"
     navBtn.textContent = "menu"
+  }
+}
+
+function changeLayout() {
+  if (window.innerWidth >= 792) {
+    navBar.style.display = "block"
+    navBtn.textContent = "menu"
+  } else {
+    navBar.style.display = "none"
   }
 }
